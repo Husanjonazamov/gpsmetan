387dd2101e5c6b0d35cf591649a1643ed6877e77
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-07-11 15:54:29 +0500

    New g4f version: 0.5.6.5 (current: 0.3.9.7) | pip install -U g4f

--- core/apps/api/views/device.py
+++ core/apps/api/views/device.py
@@ -8,7 +8,10 @@ from core.apps.api.serializers.device import CreateDeviceSerializer, ListDeviceS
 from core.apps.api.filters import DeviceFilter 
 
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.db.models import Q
+from rest_framework import status
 
 
 
@@ -26,3 +29,35 @@ class DeviceView(BaseViewSetMixin, ModelViewSet):
         "retrieve": RetrieveDeviceSerializer,
         "create": CreateDeviceSerializer,
     }
+    
+    
+    @action(detail=False, methods=['post'], url_path="search", permission_classes=[AllowAny])
+    def search(self, request):
+        search_term = request.query_params.get("search")
+        
+        if not search_term:
+            return Response({"detail": "Qidiruv so'zi yuborilmadi"}, status=400)
+        
+        queryset = self.get_queryset().filter(
+            Q(deviceId__icontains=search_term) 
+        )
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({
+            "status": True,
+            "message": "o'chirildi"
+        }, status=status.HTTP_200_OK)
+    
+    
+    def partial_update(self, request, *args, **kwargs):
+        response = super().partial_update(request, *args, **kwargs)
+        return Response(
+            response.data
+        )
+    
+    
